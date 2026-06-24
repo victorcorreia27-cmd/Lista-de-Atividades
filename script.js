c895401032b0f1ae20edaf67fc794f4875e649ae
victorcorreia27-cmd <victor.correia27@escola.pr.gov.br>
2026-06-24 12:29:45 -0300

    a

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ const form = document.getElementById('form')
 const input = document.getElementByIdClassName('input')
 const todoUl = document.getElementById('todo')
 
+const todos = JSON.parse(localStorage.getItem)
+if(todo)
 
 
